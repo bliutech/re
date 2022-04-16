584b0eb8711e06b5a1fd16538b2f296e5c5fbebf
Benson Liu <bensonhliu@gmail.com>
2022-04-16 10:21:42 -0700

    implemented basic navigation + nav bar

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,25 @@
 import React from 'react';
 import {Routes, Route} from 'react-router-dom';
 import './App.css';
+
+// pages
 import Home from './pages/Home';
 import LoginRegister from './pages/Login';
 import Camera from './pages/Camera';
+import Error404 from './pages/Error404';
+
+import Navbar from './components/Navbar';
 
 function App() {
 	return (
 		<div className='App'>
+			<Navbar />
 			<Routes>
 				<Route path='/' element={<Home />} />
+				<Route path='/home' element={<Home />} />
 				<Route path='/login' element={<LoginRegister />} />
 				<Route path='/camera' element={<Camera />} />
+				<Route path='*' element={<Error404 />} />
 			</Routes>
 		</div>
 	)
