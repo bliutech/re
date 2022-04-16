ad12cb736dca68dd7eb3b6763bd376cfc4e9a722
Do3naut <51458829+Do3naut@users.noreply.github.com>
2022-04-16 16:31:19 -0700

    fixed compilation erroes

--- server/resources/cv.py
+++ server/resources/cv.py
@@ -1,7 +1,7 @@
 import jwt
 from flask_restful import Resource, reqparse
 from flask_jwt import JWT
-from model.model_interface import imageSearch
+from model.model_interface import image_search
 
 
 class CV(Resource):
@@ -11,8 +11,7 @@ class CV(Resource):
 
     def post(self):
         data = parser.parse_args()
-        prediction, category = imageSearch()
+        output_list = image_search()
         return {
-            "object": prediction,
-            "method": category
+            "object": output_list,
         }, 201
