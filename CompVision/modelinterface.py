39b6ca023f53a8e4feef744cd0ae64dd2e5d6a57
Do3naut <51458829+Do3naut@users.noreply.github.com>
2022-04-15 23:32:25 -0700

    CV folder set up
    Along with basic wrapper script for model

---
+++ CompVision/modelinterface.py
@@ -0,0 +1,21 @@
+
+from xml.dom import NOT_FOUND_ERR
+
+
+trash_dict = {
+    "landfill": ["clothes", "shoes", "trash"],
+    "recycle": ["brown-glass", "green-glass", "white-glass", "plastic", "paper"],
+    "compost": ["biological", "cardboard"],
+    "special": ["battery", "metal"]
+}
+
+def imageSearch():
+    # Take in an image (from parameter that's currently unset)
+
+    # Collect the output from the model
+    output_str = "biological"
+    # Pair output type with disposal method
+    for key, value in trash_dict:
+        if output_str in value:
+            return (output_str, key)
+    raise NOT_FOUND_ERR
