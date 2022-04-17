f1062c7d773ded577e6a0ebd0c293526aa2a84cd
henrywangzh <henrywang3@g.ucla.edu>
2022-04-16 22:19:21 -0700

    updating stuff again

--- server/app.py
+++ server/app.py
@@ -7,12 +7,11 @@ from flask_cors import CORS
 
 from db import db
 from resources.user import User
-from resources.cv import CV
+#from resources.cv import CV
+from resources.getUser import GetUser
+from resources.leaderboard import Leaderboard
 
 import os
-from dotenv import load_dotenv
-
-load_dotenv()
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///data.db"
@@ -29,9 +28,11 @@ def create_tables():
     db.create_all()
 
 api.add_resource(User, "/register")
-api.add_resource(CV, "/img")
-
+#api.add_resource(CV, "/img")
+api.add_resource(GetUser, "/user/<string:name>")
+api.add_resource(Leaderboard, "/leaderboard")
 
 if __name__ == "__main__":
     db.init_app(app)
     app.run(port=8000, debug=True)
+
