1a29919d686590bfa0926531ca114bc12b993068
Connor Pedersen <jabbapen@gmail.com>
2022-04-17 00:54:57 -0700

    asdfasdfasfd

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -4,7 +4,6 @@ import { faBars } from '@fortawesome/free-solid-svg-icons'
 import './Navbar.css'
 import 'font-awesome/css/font-awesome.min.css'
 import { Link } from 'react-router-dom'
-
 export default function Navbar({ setUser }) {
 	const [isActive, setActive] = useState(false)
 	const navBtn = document.getElementById('nav-toggle')
