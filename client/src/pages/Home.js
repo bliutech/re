dbf6cfac43aff3e103a02b18d7e26c92a11e2039
Do3naut <51458829+Do3naut@users.noreply.github.com>
2022-04-17 01:58:53 -0700

    help
    wrapped each text block in div, structured div

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -6,20 +6,26 @@ export default function Home() {
     <>
       <h1>Welcome to Re!</h1>
       <p>Reduce, reuse, recycle with your friends!</p>
-      <h3>The smarter way to recycle</h3>
-      <p>
-        When you're about to toss out that aluminum can into the recycling bin, ever felt a doubt that you're misorganizing your trash? 
-        According to the Environmental Protection Agency, just in 2018 alone, 292.4 million tons of municipal solid waste is generated. Of those,
-        146.1 tons ended up in landfills. You can help flatten that curve with the click of a button! Our app identifies your trash and gives you
-        the proper bin to dispose of it. 
-      </p>
+      <div className="textblock">
+        <h3>The smarter way to recycle</h3>
+        <p>
+          When you're about to toss out that aluminum can into the recycling bin, ever felt a doubt that you're misorganizing your trash? 
+          According to the Environmental Protection Agency, just in 2018 alone, 292.4 million tons of municipal solid waste is generated. Of those,
+          146.1 tons ended up in landfills. You can help flatten that curve with the click of a button! Our app identifies your trash and gives you
+          the proper bin to dispose of it. 
+        </p>
+      </div>
+      <div className="textblock">
       <h3>Stronger Together</h3>
       <p>
         Everything is better with a little friendly competition. Our leaderboard tracks our app usage of all users, and records your specific stats
         compared to everyone else. Race your friends, recycle frequently, and climb to the top of the leaderboard!
       </p>
+      </div>
+      <div className="textblock">
       <h3>Let's Begin.</h3>
       <p>Head over to the Recycle tab to begin recycling!</p>
+      </div>
     </>
   );
 }
